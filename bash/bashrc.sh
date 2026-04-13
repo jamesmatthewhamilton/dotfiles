7a5733d724dc98c8c2fda2b79a3e7e30b98a5b3c
James Matthew Hamilton <j.hamilton@gatech.edu>
2026-04-13 15:15:47 -0400

    Reduced SLOC in conda install. (#9)

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -3,7 +3,12 @@ sf="${BASH_SOURCE[0]}"
 while [[ -L "$sf" ]]; do sf="$(readlink "$sf")"; done
 dir="$(cd "$(dirname "$sf")" && pwd)"
 
-# Load everything (including bashrc_conda.sh if it exists)
+# Load everything
 for f in "${HOME}"/.common/*.sh "$dir"/bashrc_*.sh "$dir"/mac_*.sh; do
     [ -f "$f" ] && source "$f"
+done
+
+# Conda init (auto-detect)
+for c in ~/Repos/conda ~/miniconda3; do
+    [ -x "$c/bin/conda" ] && eval "$("$c/bin/conda" shell.bash hook)" && break
 done
